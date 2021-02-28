03b6ed199f85317f4b51afd8aa22d4e48c441064
Henrymarks1 <58492005+Henrymarks1@users.noreply.github.com>
2021-02-27 20:39:48 -0800

    adding the pannel

--- frontend/link/lib/pages/map.dart
+++ frontend/link/lib/pages/map.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class MapPage extends StatefulWidget {
   @override
@@ -16,32 +17,37 @@ class MapPageState extends State<MapPage> {
     zoom: 13,
   );
 
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(34.014797, -118.465112),
-      tilt: 90,
-      zoom: 19.151926040649414);
-
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
+      body: Stack(
+        children: <Widget>[
+          GoogleMap(
+            mapType: MapType.normal,
+            initialCameraPosition: _kGooglePlex,
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            },
+          ),
+          SlidingUpPanel(
+              maxHeight: MediaQuery.of(context).size.height * 0.65,
+              borderRadius: BorderRadius.all(Radius.circular(25)),
+              panel: ListTile(
+                leading: ClipRRect(
+                  borderRadius: BorderRadius.circular(25),
+                  child: Image.asset(
+                    'assets/images/henry.JPG',
+                  ),
+                ),
+                title: const Text("Henry Marks"),
+                subtitle: const Text('Online.'),
+                trailing: Icon(
+                  Icons.keyboard_arrow_down,
+                  size: 35,
+                ),
+              )),
+        ],
       ),
     );
-  }
-
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
