8286fd782f5defb284c32febe9bd7b88fa05f296
Henrymarks1 <58492005+Henrymarks1@users.noreply.github.com>
2021-02-28 14:07:40 -0800

    user auth

--- backend/server/index.js
+++ backend/server/index.js
@@ -57,6 +57,61 @@ app.post("/signup", (req, res) => {
   });
 });
 
+app.post("/login", (req, res) => {
+  clientUsername = req.body.username;
+  clientPassword = req.body.password;
+
+  /*
+This is an example snippet - you should consider tailoring it
+to your service.
+*/
+
+  async function fetchGraphQL(operationsDoc, operationName, variables) {
+    const result = await fetch("http://localhost:8080/v1/graphql", {
+      method: "POST",
+      body: JSON.stringify({
+        query: operationsDoc,
+        variables: variables,
+        operationName: operationName,
+      }),
+    });
+
+    return await result.json();
+  }
+
+  const operationsDoc = `
+    query MyQuery {
+      user(where: {username: {_eq: "${clientUsername}", _is_null: false}}) {
+        password
+      }
+    }
+  `;
+
+  function fetchMyQuery() {
+    return fetchGraphQL(operationsDoc, "MyQuery", {});
+  }
+
+  async function startFetchMyQuery() {
+    const { errors, data } = await fetchMyQuery();
+
+    if (errors) {
+      res.send("Username or password not found");
+      // handle those errors like a pro
+      console.error(errors);
+    }
+
+    // do something great with this precious data
+    var userinfo = data["user"];
+    var password = userinfo[0]["password"];
+
+    bcrypt.compare(clientPassword, password, function (err, result) {
+      result == true ? res.send("authorized") : res.send("password inncorrect");
+    });
+  }
+
+  startFetchMyQuery();
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
