47d851bf9c92e8b98e5a191ce4dc9513ad56d97b
Henrymarks1 <58492005+Henrymarks1@users.noreply.github.com>
2021-03-02 10:58:02 -0800

    adding addfriends

--- frontend/link/lib/components/listtile.dart
+++ frontend/link/lib/components/listtile.dart
@@ -16,66 +16,80 @@ class UserTile extends StatelessWidget {
     var uuid = Uuid();
 
     return ListTile(
-      leading: InkWell(
-        onTap: () {
-          return user == true
-              ? showDialog<void>(
-                  context: context,
-                  barrierDismissible: false, // user must tap button!
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      title: Text('Status'),
-                      content: SingleChildScrollView(
-                        child: ListBody(children: <Widget>[
-                          Text('Change status to offline.'),
-                        ]),
-                      ),
-                      actions: <Widget>[
-                        TextButton(
-                          child: Text('Approve'),
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
+        leading: InkWell(
+          onTap: () {
+            return user == true
+                ? showDialog<void>(
+                    context: context,
+                    barrierDismissible: false, // user must tap button!
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text('Status'),
+                        content: SingleChildScrollView(
+                          child: ListBody(children: <Widget>[
+                            Text('Change status to offline.'),
+                          ]),
                         ),
-                      ],
+                        actions: <Widget>[
+                          TextButton(
+                            child: Text('Approve'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  )
+                : null;
+          },
+          child: Stack(
+            alignment: AlignmentDirectional.bottomEnd,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(25),
+                child: Image.asset(
+                  userPath ?? 'image/asset/null.png',
+                ),
+              ),
+              ClipRRect(
+                  borderRadius: BorderRadius.circular(25),
+                  child: Container(
+                    height: 17,
+                    width: 17,
+                    color:
+                        online == true ? Colors.green[800] : Colors.grey[500],
+                  )),
+            ],
+          ),
+        ),
+        title: Text(name),
+        subtitle: Text(online == true ? "Online" : "Offline"),
+        trailing: Container(
+            width: MediaQuery.of(context).size.width * .3,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                IconButton(
+                  onPressed: null,
+                  icon: Icon(
+                    Icons.group_add,
+                    size: 35,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    showSearch(
+                      context: context,
+                      delegate: AddressSearch(uuid.v4()),
                     );
                   },
-                )
-              : null;
-        },
-        child: Stack(
-          alignment: AlignmentDirectional.bottomEnd,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(25),
-              child: Image.asset(
-                userPath ?? 'image/asset/null.png',
-              ),
-            ),
-            ClipRRect(
-                borderRadius: BorderRadius.circular(25),
-                child: Container(
-                  height: 17,
-                  width: 17,
-                  color: online == true ? Colors.green[800] : Colors.grey[500],
-                )),
-          ],
-        ),
-      ),
-      title: Text(name),
-      subtitle: Text(online == true ? "Online" : "Offline"),
-      trailing: IconButton(
-        onPressed: () {
-          showSearch(
-            context: context,
-            delegate: AddressSearch(uuid.v4()),
-          );
-        },
-        icon: Icon(
-          Icons.search,
-          size: 35,
-        ),
-      ),
-    );
+                  icon: Icon(
+                    Icons.search,
+                    size: 35,
+                  ),
+                ),
+              ],
+            )));
   }
 }
